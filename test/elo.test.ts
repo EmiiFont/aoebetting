a670c5ed6fe5cd8892299ff37935e79cb97e50dd
Emilio Font <emifont01@gmail.com>
2020-09-08 16:10:42 -0400

    added elo calculator and test for our starting predictions

---
+++ test/elo.test.ts
@@ -0,0 +1,40 @@
+import { Elo, EloResult } from "../src/helpers/elo";
+import { AdvancedConsoleLogger } from "typeorm";
+
+test('ELO should be greater than starting elo when winning a match', async () => {
+
+    const playerAElo = 1100;
+    const playerBElo = 1121;
+
+    let elo = new Elo(playerAElo, playerBElo, EloResult.WIN, EloResult.LOOSE);
+    
+    let results = elo.GetNewRatings();
+   
+    expect(results[0]).toBeGreaterThan(playerAElo);
+  });
+
+
+  test('ELO should be lower than starting elo when losing a match', async () => {
+
+    const playerAElo = 1100;
+    const playerBElo = 1121;
+
+    let elo = new Elo(playerAElo, playerBElo, EloResult.LOOSE, EloResult.WIN);
+    
+    let results = elo.GetNewRatings();
+   
+    expect(results[0]).toBeLessThan(playerAElo);
+  });
+
+
+  test('predicted win should be greater for greater elo', async () => {
+
+    const playerAElo = 1100;
+    const playerBElo = 1121;
+
+    let predicted = Elo.predictResult(1121, 1100);
+    
+    
+    expect(predicted[0] > predicted[1]).toBeTruthy();
+  });
+
